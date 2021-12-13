b9070a597026bafd6328b56c548e79860667648c
Александр Чугуевский <93049461+a1998ch@users.noreply.github.com>
2021-12-13 21:52:21 +0700

    Добавил новую переменную в задании 1.1

--- Задание 1.1/Program.cs
+++ Задание 1.1/Program.cs
@@ -6,12 +6,13 @@ namespace Задание_1._1
     {
         static void Main(string[] args)
         {
-            double x, y, c; // Вводим и задаём тип переменных
+            double x, y, c, sum; // Вводим и задаём тип переменных
             Console.Write("Введите x: ");
             x = double.Parse(Console.ReadLine()); // Для ввода переменных с консоли
             Console.Write("Введите y: ");
             y = double.Parse(Console.ReadLine());
             c = Math.Atan(y / x) * 180 / Math.PI; // Определение угла
+            sum = x + y;
             Console.WriteLine("Угол: {0}", c); // Вывод результата
         }
     }
