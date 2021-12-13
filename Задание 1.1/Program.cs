4a9477b4bd9b9180eedde82b0e76c4d43928987d
Александр Чугуевский <93049461+a1998ch@users.noreply.github.com>
2021-12-13 21:27:26 +0700

    Добавьте файлы проекта.

---
+++ Задание 1.1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Задание_1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x, y, c; // Вводим и задаём тип переменных
+            Console.Write("Введите x: ");
+            x = double.Parse(Console.ReadLine()); // Для ввода переменных с консоли
+            Console.Write("Введите y: ");
+            y = double.Parse(Console.ReadLine());
+            c = Math.Atan(y / x) * 180 / Math.PI; // Определение угла
+            Console.WriteLine("Угол: {0}", c); // Вывод результата
+        }
+    }
+}
